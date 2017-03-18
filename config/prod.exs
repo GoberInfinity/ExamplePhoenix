6296941becd45a8c768ad9beb5a57b971869df17
Gober <reyesfragosoroberto@gmail.com>
2017-03-18 17:50:24 -0600

    [Added] Channel Heroku v7

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,8 @@ config :otherpool, Otherpool.Endpoint,
   url: [scheme: "https", host: "otherpoolapi", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: false
 
 # Do not print debug messages in production
 config :logger, level: :info
